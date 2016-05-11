fbfac52e4196e55fdd0ef351a3edf43df20b8e5c
d0kt0r0 <ogbornd@mcmaster.ca>
2016-05-11 12:54:17 -0400

    SampleBank throws when passed null audioContext, SampleBank.load catches exceptions in HttpRequest

--- SampleBank.js
+++ SampleBank.js
@@ -2,6 +2,7 @@
 SampleBank = function(urlPrefix,audioContext) {
   this.urlPrefix = urlPrefix;
   this.samples = {};
+  if(audioContext == null) throw Error("audioContext argument to SampleBank constructor was null");
   this.ac = audioContext;
 }
 
@@ -19,10 +20,11 @@ SampleBank.prototype.load = function(name) {
   this.samples[name] = { status: 'loading' };
   var url = this.urlPrefix + "/" + name;
   var request = new XMLHttpRequest();
-  request.open('GET',url,true);
-  request.responseType = 'arraybuffer';
-  var closure = this; // a closure is necessary for...
-  request.onload = function() {
+  try {
+    request.open('GET',url,true);
+    request.responseType = 'arraybuffer';
+    var closure = this; // a closure is necessary for...
+    request.onload = function() {
       closure.ac.decodeAudioData(request.response, function(x) {
         console.log("sample " + url + "loaded");
         closure.samples[name].buffer = x; // ...the decoded data to be kept in the object
@@ -32,8 +34,12 @@ SampleBank.prototype.load = function(name) {
         console.log("error decoding sample " + url);
         closure.samples[name].status = 'error';
       });
-  };
-  request.send();
+    };
+    request.send();
+  }
+  catch(e) {
+    console.log("exception during loading of sample " + name + ":" + e);
+  }
 }
 
 SampleBank.prototype.getBuffer = function(name) {
